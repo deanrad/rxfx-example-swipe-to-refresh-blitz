f279234c364a6884c29c91fe3fda49947af037f3
deanrad <24406+deanrad@users.noreply.github.com>
2022-10-12 14:44:25 -0500

    on timer/request, call the simulated time endpoint

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
-import { after } from '@rxfx/after';
-import './services/bus';
+import './services/time'
 
-const fakeTimeResponse = after(2500, () => {
-  return new Date().toUTCString();
-}); // .then(console.log);
 
 // Adapted from https://www.learnrxjs.io/learn-rxjs/recipes/swipe-to-refresh
 // Original code: https://stackblitz.com/edit/rxjs-refresh
