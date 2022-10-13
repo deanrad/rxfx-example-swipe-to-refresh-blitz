4b90db77238e154912f9dd2273b370e44e20d22b
deanrad <24406+deanrad@users.noreply.github.com>
2022-10-12 21:48:38 -0500

    DOM pointer up and down events are sent through pointerService

--- services/pointer.ts
+++ services/pointer.ts
@@ -2,12 +2,40 @@ import { createService } from '@rxfx/service';
 import { Observable } from 'rxjs';
 import { bus } from './bus';
 
+const MOBILE = { DOWN: 'touchstart', UP: 'touchend', MOVE: 'touchmove' };
+const DESKTOP = { DOWN: 'mousedown', UP: 'mouseup', MOVE: 'mousemove' };
+
+export const POINTER_EVENTS =
+  'ontouchstart' in document.documentElement ? MOBILE : DESKTOP;
+
 const mouseYs: Observable<number> = null;
 export const pointerService = createService<'up' | 'down', number, Error>(
   'pointer',
   bus,
   () => mouseYs
 );
+
+/////////////////// DOM Event Harvesting /////////////////
+
+// DOM listener setup + teardown
+const sendDown = (e: Event) => {
+  e.preventDefault();
+  pointerService.request('down');
+};
+const sendUp = (e: Event) => {
+  e.preventDefault();
+  pointerService.request('up');
+};
+
+export const connectDOMEventsToPointerService = () => {
+  document.addEventListener(POINTER_EVENTS.DOWN, sendDown);
+  document.addEventListener(POINTER_EVENTS.UP, sendUp);
+};
+export const disconnectDOMEventsFromPointerService = () => {
+  document.removeEventListener(POINTER_EVENTS.DOWN, sendDown);
+  document.removeEventListener(POINTER_EVENTS.UP, sendUp);
+};
+connectDOMEventsToPointerService();
 
 // Example:
 // pointerService.request('test');
