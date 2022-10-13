ff8f65112188dad26288ff9cc899b092120babc6
deanrad <24406+deanrad@users.noreply.github.com>
2022-10-12 22:11:15 -0500

    listen to pointerService to cancel drag and trigger the time refresh

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
-import './services/time';
-import './services/pointer';
+import { TIME_REQUEST } from './services/time';
+import { pointerService } from './services/pointer';
+import { bus } from './services/bus';
+
+export const isPastThreshold = (y: number) => y >= 102;
+
+// Business rules enabled by the pointerService
+pointerService.responses.subscribe(({ payload: y }) => {
+  if (isPastThreshold(y)) {
+    pointerService.cancelCurrent();
+    bus.trigger(TIME_REQUEST);
+  }
+});
 
 // Adapted from https://www.learnrxjs.io/learn-rxjs/recipes/swipe-to-refresh
 // Original code: https://stackblitz.com/edit/rxjs-refresh
